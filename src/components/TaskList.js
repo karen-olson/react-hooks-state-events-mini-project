1becf5046a63724ae0236db89841bfe39a3bf3fd
Karen Olson <karmarie@gmail.com>
2021-09-01 14:40:43 -0500

    Display all tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Task from "./Task";
+import { v4 as uuidv4 } from "uuid";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ categories, tasks }) {
+  const taskComponents = tasks.map((task) => (
+    <Task category={task.category} text={task.text} key={uuidv4()} />
+  ));
+
+  return <div className="tasks">{taskComponents}</div>;
 }
 
 export default TaskList;
